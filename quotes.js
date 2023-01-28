bc0273c1b4b830069257241293c2b1f355f11cf0
0xTak <118142360+0xTak@users.noreply.github.com>
2023-01-27 21:54:56 -0500

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,31 @@
+const quotes = [
+    { 
+        quote: "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.", 
+        author: "Brian W. Kernighan" },
+    { 
+        quote: "I’m not a great programmer; I’m just a good programmer with great habits.", 
+        author: "Kent Beck" },
+    { 
+        quote: "It’s not a bug – it’s an undocumented feature.", 
+        author: "Unknown" },
+    { 
+        quote: "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.", 
+        author: "Rick Cook" },
+    { 
+        quote: "There are two ways to write error-free programs; only the third one works.", author: "Alan J. Perlis" },
+    { 
+        quote: "If debugging is the process of removing software bugs, then programming must be the process of putting them in.", 
+        author: "Edsger W. Dijkstra" },
+    { 
+        quote: "Good code is its own best documentation. As you’re about to add a comment, ask yourself, ‘How can I improve the code so that this comment isn’t needed?’", 
+        author: "Steve McConnell" },
+    { 
+        quote: "The best thing about a boolean is even if you are wrong, you are only off by a bit.", 
+        author: "Unknown" },
+    { 
+        quote: "It’s not at all important to get it right the first time. It’s vitally important to get it right the last time.", 
+        author: "Andrew Hunt and David Thomas" },
+    { 
+        quote: "The code is like humor. When you have to explain it, it’s bad.", 
+        author: "Cory House" }
+]
